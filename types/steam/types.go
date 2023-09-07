6e4541fc61c30245d049138cf90356e9840fd6e5
246859 <2633565580@qq.com>
2023-09-07 18:18:45 +0800

    feat(steam): add ids join func

--- types/steam/types.go
+++ types/steam/types.go
@@ -1,5 +1,10 @@
 package steam
 
+import (
+	"github.com/spf13/cast"
+	"strings"
+)
+
 // CommonResponse
 // for some interface which need publisher api key are hard to test
 // and no way to know their response, so use this type to replace
@@ -13,18 +18,15 @@ type AppId struct {
 	AppId uint64 `json:"appid" mapstructure:"appid"`
 }
 
-type AppIds struct {
-	AppIds string `json:"appids" mapstructure:"appdis"`
-}
-
 type SteamId struct {
 	SteamId uint64 `json:"steamid" mapstructure:"steamid"`
 }
 
-type SteamIds struct {
-	SteamIds string `json:"steamids" mapstructure:"steamids"`
-}
-
-type SteamIdString struct {
-	SteamId string `json:"steamid" mapstructure:"steamid"`
+// Ids join several uint id with the ","
+func Ids(ids ...uint64) string {
+	var res []string
+	for _, id := range ids {
+		res = append(res, cast.ToString(id))
+	}
+	return strings.Join(res, ",")
 }
